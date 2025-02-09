380f46cd9b53342e53d1b94fe56d56283cdebba9
velzie <velzie@velzie.rip>
2025-02-09 17:33:22 -0500

    restore account saving support

--- DepotDownloader/AccountSettingsStore.cs
+++ DepotDownloader/AccountSettingsStore.cs
@@ -49,23 +49,24 @@ namespace DepotDownloader
             if (Loaded)
                 throw new Exception("Config already loaded");
 
-            // if (IsolatedStorage.FileExists(filename))
-            // {
-            //     try
-            //     {
-            //         using var fs = IsolatedStorage.OpenFile(filename, FileMode.Open, FileAccess.Read);
-            //         using var ds = new DeflateStream(fs, CompressionMode.Decompress);
-            //         Instance = Serializer.Deserialize<AccountSettingsStore>(ds);
-            //     }
-            //     catch (IOException ex)
-            //     {
-            //         Console.WriteLine("Failed to load account settings: {0}", ex.Message);
-            //         Instance = new AccountSettingsStore();
-            //     }
-            // }
-            // else
-            // {
-            Instance = new AccountSettingsStore();
+            if (File.Exists(filename))
+            {
+                try
+                {
+                    // using var ds = new DeflateStream(fs, CompressionMode.Decompress);
+                    using var fs = File.Open(filename, FileMode.Open, FileAccess.Read);
+                    Instance = Serializer.Deserialize<AccountSettingsStore>(fs);
+                }
+                catch (IOException ex)
+                {
+                    Console.WriteLine("Failed to load account settings: {0}", ex.Message);
+                    Instance = new AccountSettingsStore();
+                }
+            }
+            else
+            {
+                Instance = new AccountSettingsStore();
+            }
 
             Instance.FileName = filename;
         }
@@ -75,16 +76,15 @@ namespace DepotDownloader
             if (!Loaded)
                 throw new Exception("Saved config before loading");
 
-            // try
-            // {
-            //     using var fs = IsolatedStorage.OpenFile(Instance.FileName, FileMode.Create, FileAccess.Write);
-            //     using var ds = new DeflateStream(fs, CompressionMode.Compress);
-            //     Serializer.Serialize(ds, Instance);
-            // }
-            // catch (IOException ex)
-            // {
-            //     Console.WriteLine("Failed to save account settings: {0}", ex.Message);
-            // }
+            try
+            {
+                using var fs = File.Open(Instance.FileName, FileMode.OpenOrCreate, FileAccess.Write);
+                Serializer.Serialize(fs, Instance);
+            }
+            catch (IOException ex)
+            {
+                Console.WriteLine("Failed to save account settings: {0}", ex.Message);
+            }
         }
     }
 }
