5926c661196d3f00e8c2ac56a1d121bdf8f6a8da
velzie <velzie@velzie.rip>
2025-02-11 21:19:00 -0500

    fix a bad crypto implementation

--- SteamKit/SteamKit2/SteamKit2/NativeCrypto.c
+++ SteamKit/SteamKit2/SteamKit2/NativeCrypto.c
@@ -2,11 +2,14 @@
 #include <string.h>
 #include <stdio.h>
 #include <stdlib.h>
+/* #include <openssl/aes.h> */
 
 #  define AES_MAXNR 14
 # define AES_BLOCK_SIZE 16
 
 #  define AES_DECRYPT     0
+
+#  define AES_ENCRYPT     1
 struct aes_key_st {
 #  ifdef AES_LONG
     unsigned long rd_key[4 * (AES_MAXNR + 1)];
@@ -18,6 +21,9 @@ struct aes_key_st {
 typedef struct aes_key_st AES_KEY;
 
 int AES_set_decrypt_key(const unsigned char *userKey, const int bits,
+                        AES_KEY *key);
+
+int AES_set_encrypt_key(const unsigned char *userKey, const int bits,
                         AES_KEY *key);
 void AES_decrypt(const unsigned char *in, unsigned char *out,
                  const AES_KEY *key);
@@ -44,7 +50,33 @@ int AesDecryptEcb( uint8_t *key, int key_size, uint8_t *ciphertext, int cipherte
     memcpy( buffer, plaintext, ciphertext_length );
 
 
-    return 0;
+    return ciphertext_length;
+}
+int AesEncryptCbc( uint8_t *key, int key_size, uint8_t *iv, uint8_t *plaintext, int plaintext_length, char *buffer ) {
+
+    AES_KEY aes_key;
+    if ( AES_set_encrypt_key( key, key_size * 8, &aes_key ) != 0 ) {
+        return -1;
+    }
+
+    int pad_length = AES_BLOCK_SIZE - ( plaintext_length % AES_BLOCK_SIZE );
+    if ( pad_length == 0 ) {
+        pad_length = AES_BLOCK_SIZE;
+    }
+
+
+    uint8_t *ciphertext = malloc( plaintext_length + pad_length );
+    if ( ciphertext == NULL ) {
+        return -1;
+    }
+    memcpy( ciphertext, plaintext, plaintext_length );
+    memset( ciphertext + plaintext_length, pad_length, pad_length );
+
+    AES_cbc_encrypt( ciphertext, ciphertext, plaintext_length, &aes_key, iv, AES_ENCRYPT );
+
+    memcpy( buffer, ciphertext, plaintext_length );
+
+    return plaintext_length + pad_length;
 }
 
 int AesDecryptCbc( uint8_t *key, int key_size, uint8_t *iv, uint8_t *ciphertext, int ciphertext_length, char *buffer ) {
--- SteamKit/SteamKit2/SteamKit2/NativeCrypto.cs
+++ SteamKit/SteamKit2/SteamKit2/NativeCrypto.cs
@@ -7,4 +7,6 @@ public partial class NativeCrypto
     public static unsafe extern int AesDecryptEcb( byte[] key, int key_size, byte* ciphertext, int ciphertext_length, byte* buffer );
     [DllImport( "NativeCrypto" )]
     public static unsafe extern int AesDecryptCbc( byte[] key, int key_size, byte* iv, byte* ciphertext, int ciphertext_length, byte* buffer );
+    [DllImport( "NativeCrypto" )]
+    public static unsafe extern int AesEncryptCbc( byte[] key, int key_size, byte* iv, byte* ciphertext, int ciphertext_length, byte* buffer );
 }
